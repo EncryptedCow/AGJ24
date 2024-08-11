a7e695215623fcd7a35a2218a5921d4ef7f9ac98
Tyson Hoang <56943752+tysonkh@users.noreply.github.com>
2024-08-11 09:00:21 -0600

    Update melee enemy behaviour
    + Fix melee enemy not dealing damage when collided with
    + Melee enemy is no longer random, and will always pursue

--- components/hurtbox/hurtbox.gd
+++ components/hurtbox/hurtbox.gd
@@ -7,6 +7,13 @@ func _on_area_entered(area: Area2D) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	_test_hit(body)
 
+# specifically for the melee enemy's hurtbox
+# sends a message that the other entity was hit
+# this only procs with an entity with the correct collision/layer masks
+func _test_touch_hit(area: Area2D) -> void:
+	area.hit(self)
+	#print("OOF")
+
 func _test_hit(hit_node: Node) -> void:
 	if is_instance_of(hit_node, Hitbox):
 		if hit_node.owner == owner.origin_entity:
