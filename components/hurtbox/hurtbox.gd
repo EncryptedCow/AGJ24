2a1e428d190d29e3a5536379b6a0ab4b6d5220b0
Josh <encryptedcow@gmail.com>
2024-08-10 22:02:37 -0600

    added bullet animations and tilemap collisions

--- components/hurtbox/hurtbox.gd
+++ components/hurtbox/hurtbox.gd
@@ -2,13 +2,19 @@ class_name Hurtbox
 extends Area2D
 
 func _on_area_entered(area: Area2D) -> void:
-	if is_instance_of(area, Hitbox):
-		if area.owner == owner.origin_entity:
+	_test_hit(area)
+
+func _on_body_entered(body: Node2D) -> void:
+	_test_hit(body)
+
+func _test_hit(hit_node: Node) -> void:
+	if is_instance_of(hit_node, Hitbox):
+		if hit_node.owner == owner.origin_entity:
 			return
 		
-		var hitbox: Hitbox = area
+		var hitbox: Hitbox = hit_node
 		hitbox.hit(self)
 		owner.queue_free()
-		print("YEOWCH")
+		#print("YEOWCH")
 	else:
-		print("bullet hit terrain")
+		owner.queue_free()
